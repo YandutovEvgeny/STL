a6ff93f6452213bfda66ac2ab1c409f6cce2c7cb
yandutka@mail.ru <yandutka@mail.ru>
2022-01-18 20:18:19 +0300

    vector DONE

--- STL/STL.cpp
+++ STL/STL.cpp
@@ -2,11 +2,22 @@
 #include<array>
 #include<vector>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define tab "\t"
 
 //#define STL_ARRAY
 #define STL_VECTOR
+template<typename T>void print(const vector<T>& vec)
+{
+	for (typename vector<T>::const_iterator it = vec.begin(); it != vec.end(); it++)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+}
 
 void main()
 {
@@ -41,7 +52,8 @@ void main()
 		cerr << e.what() << endl;
 	}
 	
-	int insert_index, insert_element;
+
+	/*int insert_index, insert_element;
 	cout << "Введите индекс по которому хотите вставить значение: "; cin >> insert_index;
 	cout << "Введите значение, которое хотите вставить: "; cin >> insert_element;
 	vector<int>::iterator it;
@@ -60,12 +72,42 @@ void main()
 	{
 		cout << vec.at(i) << tab;
 	}
-	cout << endl;
+	cout << endl;*/  
 	/*vector<vector<int>> vec2;
 	cout << "Size:	   " << vec2.size() << endl;
 	cout << "Capacity: " << vec2.capacity() << endl;
-	cout << "MaxSize:  " << vec2.max_size() << endl;*/
+	cout << "MaxSize:  " << vec2.max_size() << endl;*/  
 	
+	int index;
+	int value;
+	int count;
+	cout << "Введите индекс добавляемого элемента:\t"; cin >> index;
+	cout << "Введите количество добавлений:\t\t"; cin >> count;
+	cout << "Введите значение добавляемого элемента:\t"; cin >> value;
+	//                    insert(position, count, value)
+	if (index < vec.size())vec.insert(vec.begin() + index, count, value);
+	else cout << "Error: out of range" << endl;
+	print(vec);
+	
+	std::vector<int> powers = { 1024,2048,4096,8192,16384 };
+
+	/*vec.insert(vec.begin() + 4, powers.begin(), powers.end());
+	vec.insert(vec.begin() + 3, { 256,384,512,768 });
+	for (int i : vec)cout << i << tab; cout << endl;*/
+
+	/*vec.insert(vec.begin() + 4, vec.begin() + 15, vec.begin() + 17);
+	for (int i : vec)cout << i << tab; cout << endl;*/
+
+	cout << "Введите индекс удаляемого элемента:\t"; cin >> index;
+	cout << "Введите количество добавлений:\t\t"; cin >> count;
+	if (index <= vec.size())vec.erase(vec.begin() + index, vec.begin()+index+count);
+	else cout << "Error: out of range" << endl;
+	print(vec);
+
+	cout << "\n---------------------------------------------------\n";
+	vec.swap(powers);
+	print(vec);
+	print(powers);
 #endif // STL_VECTOR
 
 }
